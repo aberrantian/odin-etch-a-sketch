d6b5044ed5f41447b6f78ffbde43432f68ee22f6
aberrantian <aberrantian@gmail.com>
2022-10-28 01:37:47 +0100

    refactor grid to function

--- main.js
+++ main.js
@@ -5,20 +5,25 @@ grid.classList.add('grid');
 
 let gridSize = 16;
 
-for (let i = 0; i < gridSize; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    
-    container.appendChild(row);
-    
-    for (let n = 0; n < gridSize; n++) {
-        const column = document.createElement('div');
-        column.classList.add('column');
-        column.addEventListener('mouseover', () => {
-            column.classList.toggle('hover');
-        })
-    
-        row.appendChild(column);
+function generateGrid(gridSize = 16) {
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        
+        container.appendChild(row);
+        
+        for (let n = 0; n < gridSize; n++) {
+            const column = document.createElement('div');
+            column.classList.add('column');
+            column.addEventListener('mouseover', () => {
+                column.classList.toggle('hover');
+            })
+        
+            row.appendChild(column);
+        };
     };
 };
+
+generateGrid();
+
 
