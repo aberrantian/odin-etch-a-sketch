05dcb3a35c4f2552caf3b46634363f19eb2a02a1
aberrantian <aberrantian@gmail.com>
2022-10-28 01:08:52 +0100

    setup css grid and color

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const container = document.querySelector('#container');
+const container = document.querySelector('.container');
 
 const grid = document.createElement('div');
 grid.classList.add('grid');
